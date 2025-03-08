1895d66284e076458d68fc780fcee5606a45a9e1
JARP <andersonandy046@gmail.com>
2025-03-09 01:55:36 +0800

    update

--- static/js/validation.js
+++ static/js/validation.js
@@ -1,28 +1,2 @@
 
 
-    $("#btnCreateAccount").click(function (e) { 
-      
-        let regName = $.trim($("#regName").val());
-        let regEmail = $.trim($("#regEmail").val());
-        let regPassword = $.trim($("#regPassword").val());
-
-        let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // Email RegEx
-
-        if (regName === "") {
-            alertify.error('Name is Required!');
-            return;
-        }
-        if (regEmail === "") {
-            alertify.error('Email is Required!');
-            return;
-        }
-        if (!emailPattern.test(regEmail)) {  // Check email format
-            alertify.error('Invalid Email Format!');
-            return;
-        }
-
-        if (regPassword === "") {
-            alertify.error('Password is Required!');
-            return;
-        }
-    });
